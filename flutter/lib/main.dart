b10c963896f7d010e0a726bb4a1f6fada7090b25
O6lvl4 <motodera@phismize.com>
2022-04-02 02:11:10 +0900

    Add greeting api and show flutter view at hello world response

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,20 +1,52 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class HelloWorldResponse {
+  final String message;
+
+  HelloWorldResponse({required this.message});
+
+  factory HelloWorldResponse.fromJson(Map<String, dynamic> json) {
+    return HelloWorldResponse(
+      message: json['message'],
+    );
+  }
+}
+
+class MyApp extends HookWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final helloWorld = useState("");
+    useEffect(() {
+      (() async {
+        final url = Uri.parse(
+            "https://gjwyot50al.execute-api.ap-northeast-1.amazonaws.com/dev/greeting");
+        final response = await http.get(url);
+        if (response.statusCode == 200) {
+          final bodyJson = json.decode(response.body);
+          final greeting = HelloWorldResponse.fromJson(bodyJson).message;
+          helloWorld.value = greeting;
+        } else {
+          debugPrint(response.body.toString());
+        }
+      })();
+      return;
+    }, []);
     return MaterialApp(
       title: 'Hello, World!',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Hello, World!'),
+      home: MyHomePage(title: helloWorld.value),
     );
   }
 }
--- greeting-api/rest/python3.9/hello_world/app.py
+++ greeting-api/rest/python3.9/hello_world/app.py
@@ -3,6 +3,11 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "GET,OPTIONS",
+            "Access-Control-Allow-Origin": "*"
+        },
         "body": json.dumps({
             "message": "Hello, World!",
         }),
